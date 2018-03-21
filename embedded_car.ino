70ae676bd6f2532dbaf14e97fd9e1499591fb80f
TaylorDaly <taylor.daly@ucdenver.edu>
2018-03-21 15:17:35 -0600

    Added state logic

--- embedded_car.ino
+++ embedded_car.ino
@@ -9,8 +9,10 @@ int speedpinA = 9; //enable motor A
 int pinI3 = 12; //define I3 interface
 int pinI4 = 13; //define I4 interface
 int speedpinB = 10; //enable motor B
-int motorSpeed = 175; //define the speed of motor
-
+int motorSpeed = 255; //define the speed of motor
+enum states {movingForward, turningLeft, turningRight, movingBackward, stopped} state;
+enum states lastState = state;
+enum states lastTurn = turningLeft;
 void setup()
 {
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
@@ -28,13 +30,22 @@ void setup()
 
 void loop()
 {
+  //get ultrasonic reading
   readUltrasonic();
+  //if wall is within 25 cm
   if (distance <= 25)
   {
-    left();
-    delay(1400);
-  }//else //forward();
-  forward();
+    if (lastTurn == turningRight) {
+      left();
+      //let turn left for .75 seconds (approx 90 degrees)
+      delay(750);
+    } else {
+      right();
+      delay(750);
+    }
+  } else {
+    forward();
+  }
   //  left();
   //  delay(2000);
   //  stop();
@@ -48,8 +59,20 @@ void loop()
   //  backward();
   //  delay(2000);
   //  stop();
+  //print state changes
+  if (state != lastState) {
+    Serial.print("State: ");
+    Serial.println(state);
+  }
+  //track previous turn
+  if (state == turningLeft || state == turningRight) {
+    lastTurn = state;
+  }
+  //track previous state
+  lastState = state;
 }
-void left()
+
+void right()
 {
   analogWrite(speedpinA, motorSpeed); //input a simulation value to set the speed
   analogWrite(speedpinB, motorSpeed);
@@ -57,8 +80,9 @@ void left()
   digitalWrite(pinI3, LOW);
   digitalWrite(pinI2, LOW); //turn DC Motor A move anticlockwise
   digitalWrite(pinI1, HIGH);
+  state = turningRight;
 }
-void backward()
+void left()
 {
   analogWrite(speedpinA, motorSpeed); //input a simulation value to set the speed
   analogWrite(speedpinB, motorSpeed);
@@ -66,6 +90,7 @@ void backward()
   digitalWrite(pinI3, HIGH);
   digitalWrite(pinI2, HIGH); //turn DC Motor A move clockwise
   digitalWrite(pinI1, LOW);
+  state = turningLeft;
 }
 void forward()
 {
@@ -75,21 +100,23 @@ void forward()
   digitalWrite(pinI3, LOW);
   digitalWrite(pinI2, HIGH); //turn DC Motor A move clockwise
   digitalWrite(pinI1, LOW);
+  state = movingForward;
 }
-void right()
+void backward()
 {
   analogWrite(speedpinA, motorSpeed); //input a simulation value to set the speed
   analogWrite(speedpinB, motorSpeed);
-  digitalWrite(pinI4, LOW); //turn DC Motor B move anticlockwise
+  digitalWrite(pinI4, LOW); //turn DC Motor B move counter clockwise
   digitalWrite(pinI3, HIGH);
-  digitalWrite(pinI2, LOW); //turn DC Motor A move clockwise
+  digitalWrite(pinI2, LOW); //turn DC Motor A move counter clockwise
   digitalWrite(pinI1, HIGH);
+  state = movingBackward;
 }
 void stop()
 {
   digitalWrite(speedpinA, LOW); // disable the speed pin to stop the motor
   digitalWrite(speedpinB, LOW);
-  //delay(1000);
+  state = stopped;
 }
 
 int readUltrasonic() {
@@ -105,7 +132,7 @@ int readUltrasonic() {
   // Calculating the distance
   distance = duration * 0.034 / 2;
   // Prints the distance on the Serial Monitor
-  Serial.print("Distance: ");
-  Serial.println(distance);
+  //Serial.print("Distance: ");
+  //Serial.println(distance);
 }
 
