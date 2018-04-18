cdc3b06bcde129126372fd6b177f8341da882e60
jordvaug <36082622+jordvaug@users.noreply.github.com>
2018-04-17 21:44:14 -0600

    Add files via upload
    This is the updated code, Taylor can feel free to move it into his file. New comments explaining the car were added, as well as some minor error corrections.

--- carGo.ino
+++ carGo.ino
@@ -1,57 +1,73 @@
-
 #include <Wire.h>
 #include <VL6180X.h>
 
-
+// defines pins for ultrasonic sensor
 #define trigPin 6
-#define echo 7
+#define echoPin 7
 #define RANGE 1
 
 /* List of adresses for each sensor - after reset the address can be configured */
 #define address0 0x22
-#define address1 0x20                                             
+#define address1 0x20 
 
-
-/* These Arduino pins must be wired to the IO0 pin of VL6180x */
+/* GPIO stands for general purpose input/output pin */
 int enablePin0 = A0;
 int enablePin1 = A1;
+
 
 //Used for the sensor
 long timer;                             //used to measure time between meausurements
 long duration;                          //used to measure the time between pulse an echo
-int USdistanceRead; 
 int laserDist0;
 int laserDist1;
+int ultrasonicDist;
 
-
-/* Create a new instance for each sensor */
+/* Create a new instance for each sensor. Sensor 0 is on the left and sensor 1 is the right, sensor 0 has the 
+GPIO pin wired to A0 and sensor1 has its SHDN pin wired to A1. The SDA and SCL of sensor 0 are connected to 
+A4 and A5(The SDA and SCL pins). Both senors GND and VIN pins are connected to their own G and V pins.*/
 VL6180X sensor0;
 VL6180X sensor1;
 
+/*Note on steering: Changing direction rapidly can cause unexpected effects. From a mechanical standpoint, going from forward
+to reverse rapidly could potentially damage a gear box.  From an electrical standpoint, it can cause large current and voltage spikes.  
+To resolve these issues, a motor needs to be taken from one direction to another with a small pause inbetween */
 
-//*note on steering: Changing direction rapidly can cause unexpected effects. From a mechanical standpoint, going from forward
-//to reverse rapidly could potentially damage a gear box.  From an electrical standpoint, it can cause large current and voltage spikes.  
-//To resolve these issues, a motor needs to be taken from one direction to another with a small pause inbetween
+/*All measurements are returned in mm*/
+ 
 
 
-void setup() {
+//Motor pins
+int pinI1 = 8; //define I1 interface
+int pinI2 = 11; //define I2 interface
+int speedpinA = 9; //enable motor A
+int pinI3 = 12; //define I3 interface
+int pinI4 = 13; //define I4 interface
+int speedpinB = 10; //enable motor B
 
-  Serial.begin(9600);
+int motorSpeed = 255; //define the speed of motor
+
+//State logic
+enum states {movingForward, turningLeft, turningRight, movingBackward, stopped} state;
+enum states lastState = state;
+enum states lastTurn = turningLeft;
+
+
+void setup()
+{
+  Serial.begin(9600); // Starts the serial communication
   Wire.begin();
 
-//US sensor
-  pinMode(echo, INPUT);
-  pinMode(trigPin, OUTPUT);
-  
-  // Reset all connected sensors
+  //Ultrasonic sensor
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+
+  // Reset all connected laser sensors
   pinMode(enablePin0,OUTPUT);
   pinMode(enablePin1,OUTPUT);
 
-  
-  digitalWrite(enablePin0, LOW);        //both pins must be set to low before they are set to high
+  //both pins must be set to low before they are set to high
+  digitalWrite(enablePin0, LOW);        
   digitalWrite(enablePin1, LOW);
-
-
 
   // Sensor 0
   Serial.println("Start Sensor 0");
@@ -77,96 +93,118 @@ void setup() {
   sensor1.init();
   sensor1.configureDefault();
   sensor1.setAddress(address1);
-  Serial.println(sensor1.readReg(address0),HEX);
+  Serial.println(sensor1.readReg(address1));
   sensor1.writeReg(VL6180X::SYSRANGE__MAX_CONVERGENCE_TIME, 30);
   sensor1.writeReg16Bit(VL6180X::SYSALS__INTEGRATION_PERIOD, 50);
   sensor1.setTimeout(500);
   sensor1.stopContinuous();
   sensor1.setScaling(RANGE);
   delay(300);
-  sensor1.startInterleavedContinuous(100);
+  sensor1.startInterleavedContinuous(100);        //sets the rate at which readings are taken.
   delay(100);
 
+  //Set motor pins
+  pinMode(pinI1, OUTPUT);
+  pinMode(pinI2, OUTPUT);
+  pinMode(speedpinA, OUTPUT);
+  pinMode(pinI3, OUTPUT);
+  pinMode(pinI4, OUTPUT);
+  pinMode(speedpinB, OUTPUT);
   
-  //Setup Channel A
-  pinMode(12, OUTPUT); //Initiates Motor Channel A pin
-  pinMode(9, OUTPUT); //Initiates Brake Channel A pin
-
-  //Setup Channel B
-  pinMode(13, OUTPUT); //Initiates Motor Channel B pin
-  pinMode(8, OUTPUT);  //Initiates Brake Channel B pin
-
+  //Initial direction
+  forward();
 }
 
-void loop(){
-
-   //lasers-----------------------------------
-  //in mm
+void loop()
+{
+  //get ultrasonic reading
+  readUltrasonic();
   laserDist0 = sensor0.readRangeContinuousMillimeters();
   laserDist1 = sensor1.readRangeContinuousMillimeters();
 
-  //US distance reading in mm
-  distance();
+  //if wall is within 250 mm
+  if (ultrasonicDist <= 250)
+  {
+    if (lastTurn == turningRight) {
+      left();
+      //let turn left for .75 seconds (approx 90 degrees)
+      delay(750);
+    } else {
+      right();
+      delay(750);
+    }
+  } else {
+    forward();
+  }
 
- 
-Serial.println(USdistanceRead);
-Serial.println(laserDist0);
-Serial.println(laserDist1);
-
-
-/*
-  
-  //Motor A forward @ full speed
-  digitalWrite(12, HIGH); //Establishes forward direction of Channel A
-  digitalWrite(9, LOW);   //Disengage the Brake for Channel A
-  analogWrite(3, 255);   //Spins the motor on Channel A at full speed
-
-  //Motor B backward @ half speed
-  digitalWrite(13, LOW);  //Establishes backward direction of Channel B
-  digitalWrite(8, LOW);   //Disengage the Brake for Channel B
-  analogWrite(11, 123);    //Spins the motor on Channel B at half speed
-
-  
-  delay(3000);
-
-  
-  digitalWrite(9, HIGH);  //Engage the Brake for Channel A
-  digitalWrite(9, HIGH);  //Engage the Brake for Channel B
-
-
-  delay(1000);
-  
-  
-  //Motor A forward @ full speed
-  digitalWrite(12, LOW);  //Establishes backward direction of Channel A
-  digitalWrite(9, LOW);   //Disengage the Brake for Channel A
-  analogWrite(3, 123);    //Spins the motor on Channel A at half speed
-  
-  //Motor B forward @ full speed
-  digitalWrite(13, HIGH); //Establishes forward direction of Channel B
-  digitalWrite(8, LOW);   //Disengage the Brake for Channel B
-  analogWrite(11, 255);   //Spins the motor on Channel B at full speed
-  
-  
-  delay(3000);
-  
-  
-  digitalWrite(9, HIGH);  //Engage the Brake for Channel A
-  digitalWrite(9, HIGH);  //Engage the Brake for Channel B
-  */
-  
-  delay(1000);
-  
+  //print state changes
+  if (state != lastState) {
+    Serial.print("State: ");
+    Serial.println(state);
+  }
+  //track previous turn
+  if (state == turningLeft || state == turningRight) {
+    lastTurn = state;
+  }
+  //track previous state
+  lastState = state;
+  delay(500);
+  Serial.print("Ultrasonic Dist: ");
+  Serial.println(ultrasonicDist);
+  Serial.print("Laser Dist 0: ");
+  Serial.println(laserDist0);
+  Serial.print("Laser Dist 1: ");
+  Serial.println(laserDist1);
 }
 
+void right()
+{
+  analogWrite(speedpinA, motorSpeed); //input a simulation value to set the speed
+  analogWrite(speedpinB, motorSpeed);
+  digitalWrite(pinI4, HIGH); //turn DC Motor B move clockwise
+  digitalWrite(pinI3, LOW);
+  digitalWrite(pinI2, LOW); //turn DC Motor A move anticlockwise
+  digitalWrite(pinI1, HIGH);
+  state = turningRight;
+}
+void left()
+{
+  analogWrite(speedpinA, motorSpeed); //input a simulation value to set the speed
+  analogWrite(speedpinB, motorSpeed);
+  digitalWrite(pinI4, LOW); //turn DC Motor B move anticlockwise
+  digitalWrite(pinI3, HIGH);
+  digitalWrite(pinI2, HIGH); //turn DC Motor A move clockwise
+  digitalWrite(pinI1, LOW);
+  state = turningLeft;
+}
+void forward()
+{
+  analogWrite(speedpinA, motorSpeed); //input a simulation value to set the speed
+  analogWrite(speedpinB, motorSpeed);
+  digitalWrite(pinI4, HIGH); //turn DC Motor B move clockwise
+  digitalWrite(pinI3, LOW);
+  digitalWrite(pinI2, HIGH); //turn DC Motor A move clockwise
+  digitalWrite(pinI1, LOW);
+  state = movingForward;
+}
+void backward()
+{
+  analogWrite(speedpinA, motorSpeed); //input a simulation value to set the speed
+  analogWrite(speedpinB, motorSpeed);
+  digitalWrite(pinI4, LOW); //turn DC Motor B move counter clockwise
+  digitalWrite(pinI3, HIGH);
+  digitalWrite(pinI2, LOW); //turn DC Motor A move counter clockwise
+  digitalWrite(pinI1, HIGH);
+  state = movingBackward;
+}
+void stop()
+{
+  digitalWrite(speedpinA, LOW); // disable the speed pin to stop the motor
+  digitalWrite(speedpinB, LOW);
+  state = stopped;
+}
 
-
-/**
-* US distance readings in mm
-*/
-void distance(){  
-
-//US sensor ----------------------------------
+int readUltrasonic() {
   long pingTime=millis();
   
   digitalWrite(trigPin, HIGH);
@@ -175,12 +213,12 @@ void distance(){
   }
   digitalWrite(trigPin, LOW);
   if((millis() - pingTime)>.2){
-    USdistanceRead = 48;
+    ultrasonicDist = 4000;
   }
   else{
-  duration = pulseIn(echo, HIGH); 
-  USdistanceRead = (duration *.0066929)*25.24;    //convert to mm
+  duration = pulseIn(echoPin, HIGH); 
+  ultrasonicDist = (duration *.0066929)*25.24;    //convert to mm
   }
 
- 
 }
+
