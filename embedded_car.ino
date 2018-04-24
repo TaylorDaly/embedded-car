099fd9816e6eec8e7df39c016ed64bf346f71952
chcorona <christopher.corona@ucdenver.edu>
2018-04-24 11:17:48 -0600

    going straight
    added the correction for going straight... it was tested on hardwood floor

--- embedded_car.ino
+++ embedded_car.ino
@@ -10,7 +10,7 @@
 #define address0 0x22
 #define address1 0x20 
 
-/* These Arduino pins must be wired to the IO0 pin of VL6180x */
+/* GPIO stands for general purpose input/output pin */
 int enablePin0 = A0;
 int enablePin1 = A1;
 
@@ -22,13 +22,18 @@ int laserDist0;
 int laserDist1;
 int ultrasonicDist;
 
-/* Create a new instance for each sensor */
+/* Create a new instance for each sensor. Sensor 0 is on the left and sensor 1 is the right, sensor 0 has the 
+GPIO pin wired to A0 and sensor1 has its SHDN pin wired to A1. The SDA and SCL of sensor 0 are connected to 
+A4 and A5(The SDA and SCL pins). Both senors GND and VIN pins are connected to their own G and V pins.*/
 VL6180X sensor0;
 VL6180X sensor1;
 
-//*note on steering: Changing direction rapidly can cause unexpected effects. From a mechanical standpoint, going from forward
-//to reverse rapidly could potentially damage a gear box.  From an electrical standpoint, it can cause large current and voltage spikes.  
-//To resolve these issues, a motor needs to be taken from one direction to another with a small pause inbetween
+/*Note on steering: Changing direction rapidly can cause unexpected effects. From a mechanical standpoint, going from forward
+to reverse rapidly could potentially damage a gear box.  From an electrical standpoint, it can cause large current and voltage spikes.  
+To resolve these issues, a motor needs to be taken from one direction to another with a small pause inbetween */
+
+/*All measurements are returned in mm*/
+ 
 
 
 //Motor pins
@@ -39,10 +44,12 @@ int pinI3 = 12; //define I3 interface
 int pinI4 = 13; //define I4 interface
 int speedpinB = 10; //enable motor B
 
-int motorSpeed = 255; //define the speed of motor
+int motorSpeed = 175; //define the speed of motor
+int turnSpeed = 150;
+int correctionSpeed = 100;
 
 //State logic
-enum states {movingForward, turningLeft, turningRight, movingBackward, stopped} state;
+enum states {movingForward, turningLeft, correctLeft, turningRight, correctRight, movingBackward, stopped} state;
 enum states lastState = state;
 enum states lastTurn = turningLeft;
 
@@ -56,11 +63,12 @@ void setup()
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
 
-  // Reset all connected sensors
+  // Reset all connected laser sensors
   pinMode(enablePin0,OUTPUT);
   pinMode(enablePin1,OUTPUT);
 
-  digitalWrite(enablePin0, LOW);        //both pins must be set to low before they are set to high
+  //both pins must be set to low before they are set to high
+  digitalWrite(enablePin0, LOW);        
   digitalWrite(enablePin1, LOW);
 
   // Sensor 0
@@ -87,14 +95,14 @@ void setup()
   sensor1.init();
   sensor1.configureDefault();
   sensor1.setAddress(address1);
-  Serial.println(sensor1.readReg(address1),HEX);
+  Serial.println(sensor1.readReg(address1));
   sensor1.writeReg(VL6180X::SYSRANGE__MAX_CONVERGENCE_TIME, 30);
   sensor1.writeReg16Bit(VL6180X::SYSALS__INTEGRATION_PERIOD, 50);
   sensor1.setTimeout(500);
   sensor1.stopContinuous();
   sensor1.setScaling(RANGE);
   delay(300);
-  sensor1.startInterleavedContinuous(100);
+  sensor1.startInterleavedContinuous(100);        //sets the rate at which readings are taken.
   delay(100);
 
   //Set motor pins
@@ -113,63 +121,163 @@ void loop()
 {
   //get ultrasonic reading
   readUltrasonic();
+  
+  //get laser readings 
   laserDist0 = sensor0.readRangeContinuousMillimeters();
   laserDist1 = sensor1.readRangeContinuousMillimeters();
 
-  //if wall is within 25 cm
-  if (ultrasonicDist <= 25)
+//making a right turn on a 90 degree turn with no opening
+//to the front and an opening to the right
+
+  if(ultrasonicDist <= 175 && laserDist1 >= 150) //gonna have to measure the ultrasonic sensor so the turn will no hit the walls
   {
-    if (lastTurn == turningRight) {
-      left();
-      //let turn left for .75 seconds (approx 90 degrees)
-      delay(750);
-    } else {
-      right();
-      delay(750);
-    }
-  } else {
-    forward();
+        stop();
+        do{
+          readUltrasonic();  //reading the ultrasonic while in the loop
+          right();
+          delay(100);
+          if(ultrasonicDist >= 200) //once the ultrasonic is reading a long distance it will stop
+          {
+            state=movingForward;
+          }
+        }
+        while(state == turningRight);
+        forward();
   }
 
+//making a left turn on a 90 degree turn with no opening
+//to the front and an opening to the left
+
+  if(ultrasonicDist <= 175 && laserDist0 >= 150) //gonna have to measure the ultrasonic sensor so the turn will no hit the walls
+  {
+        stop();
+        do{
+          readUltrasonic(); //reading the ultrasonic while in the loop
+          left();
+          delay(100);
+          if(ultrasonicDist >= 200) //once the ultrasonic is reading a long distance it will stop
+          {
+            state=movingForward;
+          }
+        }
+        while(state == turningLeft);
+        forward();      
+  }  
+
+//***** might be used with the new part
+//  //if wall is within 250 mm
+//  if (ultrasonicDist <= 200)
+//  {
+//    if (lastTurn == turningRight) {
+//      left();
+//      //let turn left for .75 seconds (approx 90 degrees)
+//      delay(750);
+//    } else {
+//      right();
+//      delay(750);
+//    }
+//  } else {
+//    forward();
+//  }
+
+// correcting the car when getting close to the wall on the left
+// this only works if the course is 10 inches wide
+
+//***** this is a work in progress since the correcting over adjusts the correction ****
+  if(laserDist0 <= 25 && state == movingForward)
+  {
+      int temp = laserDist0;
+      stop();
+      do{
+          laserDist0 = sensor0.readRangeContinuousMillimeters(); //reading the laser sensor while in the loop 
+          slightRight();
+          delay(75);
+          if(laserDist0 > temp) //once the laser reading is greater then the intial reading of getting close to the wall
+          {
+            state=movingForward;
+          }
+      }while(state == correctRight);
+      forward();
+  }
+  
+// correcting the car when getting close to the wall on the right
+// this only works if the course is 10 inches wide
+  if(laserDist1 <= 50 && state == movingForward)
+  {
+      int temp = laserDist1;
+      stop();
+      do{
+          laserDist1 = sensor1.readRangeContinuousMillimeters(); //reading the laser sensor while in the loop
+          slightLeft();
+          delay(75);
+          if(laserDist1 > temp) //once the laser reading is greater then the intial reading of getting close to the wall
+          {
+            state=movingForward;
+          }
+      }while(state == correctLeft);
+      forward();
+  }
+
+  
   //print state changes
   if (state != lastState) {
     Serial.print("State: ");
     Serial.println(state);
   }
-  //track previous turn
-  if (state == turningLeft || state == turningRight) {
-    lastTurn = state;
-  }
+//  //track previous turn
+//  if (state == turningLeft || state == turningRight) {
+//    lastTurn = state;
+//  }
   //track previous state
   lastState = state;
   delay(500);
   Serial.print("Ultrasonic Dist: ");
   Serial.println(ultrasonicDist);
-  Serial.print("Laser Dist 0: ");
+  Serial.print("Laser Dist 0: "); 
   Serial.println(laserDist0);
   Serial.print("Laser Dist 1: ");
-  Serial.println(laserDist1);
+  Serial.println(laserDist1); // it is dividing by two since sensor laser 1 is reading in data at double the values
 }
 
 void right()
 {
-  analogWrite(speedpinA, motorSpeed); //input a simulation value to set the speed
-  analogWrite(speedpinB, motorSpeed);
+  analogWrite(speedpinA, turnSpeed); //input a simulation value to set the speed
+  analogWrite(speedpinB, turnSpeed);
   digitalWrite(pinI4, HIGH); //turn DC Motor B move clockwise
   digitalWrite(pinI3, LOW);
   digitalWrite(pinI2, LOW); //turn DC Motor A move anticlockwise
   digitalWrite(pinI1, HIGH);
   state = turningRight;
 }
+void slightRight()
+{
+  analogWrite(speedpinA, correctionSpeed); //input a simulation value to set the speed
+  analogWrite(speedpinB, correctionSpeed);
+  digitalWrite(pinI4, HIGH); //turn DC Motor B move clockwise
+  digitalWrite(pinI3, LOW);
+  digitalWrite(pinI2, LOW); //turn DC Motor A move anticlockwise
+  digitalWrite(pinI1, HIGH);
+  state = correctRight;
+}
 void left()
 {
-  analogWrite(speedpinA, motorSpeed); //input a simulation value to set the speed
-  analogWrite(speedpinB, motorSpeed);
+  analogWrite(speedpinA, turnSpeed); //input a simulation value to set the speed
+  analogWrite(speedpinB, turnSpeed);
   digitalWrite(pinI4, LOW); //turn DC Motor B move anticlockwise
   digitalWrite(pinI3, HIGH);
   digitalWrite(pinI2, HIGH); //turn DC Motor A move clockwise
   digitalWrite(pinI1, LOW);
   state = turningLeft;
+}
+void slightLeft()
+{
+  analogWrite(speedpinA, correctionSpeed); //input a simulation value to set the speed
+  analogWrite(speedpinB, correctionSpeed);
+  digitalWrite(pinI4, LOW); //turn DC Motor B move anticlockwise
+  digitalWrite(pinI3, HIGH);
+  digitalWrite(pinI2, HIGH); //turn DC Motor A move clockwise
+  digitalWrite(pinI1, LOW);
+  state = correctLeft;
 }
 void forward()
 {
@@ -215,4 +323,3 @@ int readUltrasonic() {
   }
 
 }
-
